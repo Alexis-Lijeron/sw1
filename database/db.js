24150dd68f72fc625f0aa2ff4e593b99d16c49d2
Alexis-lijeron <alexislijeron.a@gmail.com>
2025-01-24 13:54:15 -0400

    as

--- database/db.js
+++ database/db.js
@@ -1,15 +1,15 @@
 //2 - Invocamos a pg y realizamos la conexión
 const { Pool } = require('pg');
 const pool = new Pool({
-    // Con variables de entorno
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASS,
-    database : process.env.DB_DATABASE,
-    port     : process.env.DB_PORT,  // Asegúrate de definir el puerto si es necesario
-    ssl: {
-        rejectUnauthorized: false // Esto permite conexiones SSL sin necesidad de un certificado verificado
-      }
+  // Con variables de entorno
+  host: 'dpg-cu9qt7t6l47c73ad7tfg-a',
+  user: 'sw1_user',
+  password: 'cDer7803YDUhFfiFKdm7NvqJCY0QpkeO ',
+  database: 'sw1',
+  port: 5432,  // Asegúrate de definir el puerto si es necesario
+  ssl: {
+    rejectUnauthorized: false // Esto permite conexiones SSL sin necesidad de un certificado verificado
+  }
 });
 
 // Verificamos la conexión
