2be2ea6cb8f728d191daf892f91eb325ca9fe044
Alexis-lijeron <alexislijeron.a@gmail.com>
2025-01-24 13:58:09 -0400

    s

--- database/db.js
+++ database/db.js
@@ -2,14 +2,12 @@
 const { Pool } = require('pg');
 const pool = new Pool({
   // Con variables de entorno
-  host: 'dpg-cu9qt7t6l47c73ad7tfg-a',
-  user: 'sw1_user',
-  password: 'cDer7803YDUhFfiFKdm7NvqJCY0QpkeO ',
-  database: 'sw1',
+  host: 'localhost',
+  user: 'tu_usuario',
+  password: 'tu_contraseña ',
+  database: 'tu_base_de_datos',
   port: 5432,  // Asegúrate de definir el puerto si es necesario
-  ssl: {
-    rejectUnauthorized: false // Esto permite conexiones SSL sin necesidad de un certificado verificado
-  }
+  ssl: false,
 });
 
 // Verificamos la conexión
